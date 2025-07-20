9aebf75381edc621cb37020f036ac4b675f93780
mattravenhall <mattravenhall@users.noreply.github.com>
2025-07-20 17:23:17 +0100

    Visual fixes and map bound limits

--- js/map.js
+++ js/map.js
@@ -1,4 +1,10 @@
-const map = L.map('map').setView([20, 0], 2);
+const map = L.map('map', {
+    maxBounds: [
+        [-90, -180],
+        [90, 180]
+    ],
+    minZoom: 2
+}).setView([20, 0], 2);
 
 const darkTiles = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
